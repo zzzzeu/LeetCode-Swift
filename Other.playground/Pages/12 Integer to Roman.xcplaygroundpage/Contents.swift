7b5bd327eb60616bbbfa78cafc070188120b05bd
zzzzeu <zouanei@outlook.com>
2018-11-03 18:30:43 +0800

    12: Integer to Roman

---
+++ Other.playground/Pages/12 Integer to Roman.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+class Solution {
+    func intToRoman(_ num: Int) -> String {
+        var num = num
+        var res = ""
+        let dic = [1000: "M", 900: "CM", 500: "D", 400: "CD", 100: "C", 90: "XC", 50: "L", 40: "XL", 10: "X", 9: "IX", 5: "V", 4: "IV", 1: "I"]
+        for i in dic.keys.sorted(by: >) {
+            while num >= i {
+                num -= i
+                res += dic[i]!
+            }
+            
+        }
+        return res
+    }
+}
