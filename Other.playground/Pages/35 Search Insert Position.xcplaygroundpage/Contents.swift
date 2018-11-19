7a44898a28c682aea0a9c47cd386aa13322daf88
zzzzeu <zouanei@outlook.com>
2018-11-19 15:00:22 +0800

    35: Search Insert Position

---
+++ Other.playground/Pages/35 Search Insert Position.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+class Solution {
+    func searchInsert(_ nums: [Int], _ target: Int) -> Int {
+        var low = 0
+        var high = nums.count - 1
+        while low <= high {
+            let mid = (low + high) / 2
+            if nums[mid] == target {
+                return mid
+            } else if nums[mid] > target {
+                high = mid - 1
+            } else {
+                low = mid + 1
+            }
+        }
+        return low
+    }
+}
