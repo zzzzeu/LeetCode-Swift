83a57d0d65b04caa5d150c3e29c5282502552436
zzzzeu <zouanei@outlook.com>
2018-12-02 20:21:37 +0800

    120: Triangle

---
+++ Other.playground/Pages/120 Triangle.xcplaygroundpage/Contents.swift
@@ -0,0 +1,14 @@
+class Solution {
+    func minimumTotal(_ triangle: [[Int]]) -> Int {
+        if triangle.count == 0 {
+            return 0
+        }
+        var triangle = triangle
+        for i in (0..<triangle.count - 1).reversed() {
+            for j in triangle[i].indices {
+                triangle[i][j] += min(triangle[i + 1][j], triangle[i + 1][j + 1])
+            }
+        }
+        return triangle[0][0]
+    }
+}
