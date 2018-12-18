86ed0c22a3438a5c569ce60d88a97eedb6bb0700
zzzzeu <zouanei@outlook.com>
2018-12-19 00:44:37 +0800

    chore: improve code style

--- ListNode.playground/Pages/21 Merge Two Sorted Lists.xcplaygroundpage/Contents.swift
+++ ListNode.playground/Pages/21 Merge Two Sorted Lists.xcplaygroundpage/Contents.swift
@@ -1,18 +1,14 @@
 class Solution {
     func mergeTwoLists(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
-        if l1 == nil {
-            return l2
-        }
-        if l2 == nil {
-            return l1
-        }
+        guard let l1 = l1 else { return l2 }
+        guard let l2 = l2 else { return l1 }
         var result: ListNode? = nil
-        if l1!.val < l2!.val {
+        if l1.val < l2.val {
             result = l1
-            result?.next = mergeTwoLists(l1?.next, l2)
+            result?.next = mergeTwoLists(l1.next, l2)
         } else {
             result = l2
-            result?.next = mergeTwoLists(l1, l2?.next)
+            result?.next = mergeTwoLists(l1, l2.next)
         }
         return result
     }
