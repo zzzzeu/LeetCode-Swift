f988267803adb3e38054d7729f3ed8ba58a487c3
zzzzeu <zouanei@outlook.com>
2019-01-05 21:55:28 +0800

    263: Ugly Number

---
+++ Other.playground/Pages/263 Ugly Number.xcplaygroundpage/Contents.swift
@@ -0,0 +1,18 @@
+class Solution {
+    func isUgly(_ num: Int) -> Bool {
+        if num < 1 {
+            return false
+        }
+        var num = num
+        while num % 2 == 0 {
+            num /= 2
+        }
+        while num % 3 == 0 {
+            num /= 3
+        }
+        while num % 5 == 0 {
+            num /= 5
+        }
+        return num == 1
+    }
+}
