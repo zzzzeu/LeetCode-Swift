8dea263539ff08ae6f6846119de901d82a461e0b
zzzzeu <zouanei@outlook.com>
2019-01-08 15:21:02 +0800

    258: Add Digits

---
+++ Other.playground/Pages/258 Add Digits.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+class Solution {
+    func addDigits(_ num: Int) -> Int {
+        return result(num)
+    }
+    func result(_ num: Int) -> Int {
+        if num < 10 {
+            return num
+        }
+        var num = num
+        var sum = 0
+        while num != 0 {
+            sum += num % 10
+            num /= 10
+        }
+        return result(sum)
+    }
+}
