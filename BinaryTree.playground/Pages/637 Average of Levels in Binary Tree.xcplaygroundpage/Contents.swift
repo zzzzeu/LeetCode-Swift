58a589cce8b3d53cc86a31fab2bed78d19d0372e
zzzzeu <zouanei@outlook.com>
2019-01-27 14:23:03 +0800

    637: Average of Levels in Binary Tree

---
+++ BinaryTree.playground/Pages/637 Average of Levels in Binary Tree.xcplaygroundpage/Contents.swift
@@ -0,0 +1,27 @@
+class Solution {
+    func averageOfLevels(_ root: TreeNode?) -> [Double] {
+        var tree = [TreeNode]()
+        var res = [Double]()
+        guard let root = root else {
+            return res
+        }
+        tree.append(root)
+        var node = root
+        while !tree.isEmpty {
+            var level = 0.0
+            let count = tree.count
+            for _ in 0..<count {
+                node = tree.removeFirst()
+                level += Double(node.val)
+                if let left = node.left {
+                    tree.append(left)
+                }
+                if let right = node.right {
+                    tree.append(right)
+                }
+            }
+            res.append(level / Double(count))
+        }
+        return res
+    }
+}
