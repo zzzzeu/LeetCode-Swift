6086200a03096a56e949ce9510d89caef970f89b
zzzzeu <zouanei@outlook.com>
2019-01-30 17:21:54 +0800

    chore: improve code style

--- BinaryTree.playground/Pages/100 Same Tree.xcplaygroundpage/Contents.swift
+++ BinaryTree.playground/Pages/100 Same Tree.xcplaygroundpage/Contents.swift
@@ -1,11 +1,11 @@
 class Solution {
     func isSameTree(_ p: TreeNode?, _ q: TreeNode?) -> Bool {
-        if p == nil && q == nil {
+        if p?.val == q?.val {
             return true
         }
         if (p != nil && q == nil) || (p == nil && q != nil) || (p!.val != q!.val) {
             return false
         }
-        return isSameTree(p?.left, q?.left) && isSameTree(p?.right, q?.right)
+        return p?.val == q?.val && isSameTree(p?.left, q?.left) && isSameTree(p?.right, q?.right)
     }
 }
--- BinaryTree.playground/Pages/617 Merge Two Binary Trees.xcplaygroundpage/Contents.swift
+++ BinaryTree.playground/Pages/617 Merge Two Binary Trees.xcplaygroundpage/Contents.swift
@@ -1,9 +1,9 @@
 class Solution {
     func mergeTrees(_ t1: TreeNode?, _ t2: TreeNode?) -> TreeNode? {
-        if t1 != nil && t2 != nil {
-            t1!.val += t2!.val
-            t1?.left = mergeTrees(t1?.left, t2?.left)
-            t1?.right = mergeTrees(t1?.right, t2?.right)
+        if let t1 = t1, let t2 = t2 {
+            t1.val += t2.val
+            t1.left = mergeTrees(t1.left, t2.left)
+            t1.right = mergeTrees(t1.right, t2.right)
             return t1
         } else if t1 == nil && t2 != nil {
             return t2
