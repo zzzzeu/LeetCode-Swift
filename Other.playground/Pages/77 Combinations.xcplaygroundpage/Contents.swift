b651b00bce1e7a5f89dc8705178384b3a6d14b37
zzzzeu <zouanei@outlook.com>
2019-02-05 01:08:58 +0800

    77: Combinations

---
+++ Other.playground/Pages/77 Combinations.xcplaygroundpage/Contents.swift
@@ -0,0 +1,18 @@
+class Solution {
+    func combine(_ n: Int, _ k: Int) -> [[Int]] {
+        var res = [[Int]](), list = [Int]()
+        dfs(&res, &list, n, k, 1)
+        return res
+    }
+    func dfs(_ res: inout [[Int]], _ list: inout [Int], _ n: Int, _ k: Int, _ start: Int) {
+        if list.count == k {
+            res.append(list)
+            return
+        }
+        for i in start..<n + 1 {
+            list.append(i)
+            dfs(&res, &list, n, k, i + 1)
+            list.removeLast()
+        }
+    }
+}
