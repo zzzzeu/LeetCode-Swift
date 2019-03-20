3602d8200ab9567ec5cc5f394f01d01890f914ee
zzzzeu <zouanei@outlook.com>
2019-03-20 21:29:27 +0800

    73: Set Matrix Zeroes

---
+++ Other.playground/Pages/73 Set Matrix Zeroes.xcplaygroundpage/Contents.swift
@@ -0,0 +1,24 @@
+class Solution {
+    func setZeroes(_ matrix: inout [[Int]]) {
+        var rows = [Int]()
+        var cols = [Int]()
+        for i in matrix.indices {
+            for j in matrix[i].indices {
+                if matrix[i][j] == 0 {
+                    rows.append(i)
+                    cols.append(j)
+                }
+            }
+        }
+        for col in cols {
+            for i in matrix.indices {
+                matrix[i][col] = 0
+            }
+        }
+        for row in rows {
+            for j in matrix[row].indices {
+                matrix[row][j] = 0
+            }
+        }
+    }
+}
