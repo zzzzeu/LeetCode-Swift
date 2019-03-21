2960d2f75a08b56e4727b67bc4ab46ddf87f3b41
zzzzeu <zouanei@outlook.com>
2019-03-21 20:54:38 +0800

    99: Recover Binary Search Tree

---
+++ BinaryTree.playground/Pages/99 Recover Binary Search Tree.xcplaygroundpage/Contents.swift
@@ -0,0 +1,28 @@
+class Solution {
+    var bigger: TreeNode? = nil
+    var smaller: TreeNode? = nil
+    var node = TreeNode(Int.min)
+    func recoverTree(_ root: TreeNode?) {
+        if root == nil {
+            return
+        }
+        inorderTraverse(root)
+        let temp = smaller!.val
+        smaller!.val = bigger!.val
+        bigger!.val = temp
+    }
+    func inorderTraverse(_ root: TreeNode?) {
+        guard let root = root else {
+            return
+        }
+        inorderTraverse(root.left)
+        if bigger == nil && root.val <= node.val {
+            bigger = node
+        }
+        if root.val <= node.val {
+            smaller = root
+        }
+        node = root
+        inorderTraverse(root.right)
+    }
+}
