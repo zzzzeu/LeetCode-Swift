e488e0dacc8659a471cfebdac6bd3f9bfbd80cff
zzzzeu <zouanei@outlook.com>
2019-03-23 21:28:52 +0800

    37: Sudoku Solver

---
+++ Other.playground/Pages/37 Sudoku Solver.xcplaygroundpage/Contents.swift
@@ -0,0 +1,45 @@
+class Solution {
+    func solveSudoku(_ board: inout [[Character]]) {
+        helper(&board)
+    }
+    func helper(_ board: inout [[Character]]) -> Bool {
+        for i in board.indices {
+            for j in board[0].indices {
+                if board[i][j] == "." {
+                    for num in 1...9 {
+                        if isValid(board, i, j, Character(String(num))) {
+                            board[i][j] = Character(String(num))
+                            if helper(&board) {
+                                return true
+                            } else {
+                                board[i][j] = "."
+                            }
+                        }
+                    }
+                    return false
+                }
+            }
+        }
+        return true
+    }
+    func isValid(_ board: [[Character]], _ i: Int, _ j: Int, _ num: Character) -> Bool {
+        for x in board.indices {
+            if board[i][x] == num {
+                return false
+            }
+        }
+        for y in board[0].indices {
+            if board[y][j] == num {
+                return false
+            }
+        }
+        for x in i / 3 * 3..<i / 3 * 3 + 3 {
+            for y in j / 3 * 3..<j / 3 * 3 + 3 {
+                if board[x][y] == num {
+                    return false
+                }
+            }
+        }
+        return true
+    }
+}
