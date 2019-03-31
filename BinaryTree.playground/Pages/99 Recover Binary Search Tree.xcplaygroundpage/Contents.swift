e544ddb821bd7735ca83603dbbf85c600f963060
zzzzeu <zouanei@outlook.com>
2019-03-31 21:07:43 +0800

    chore: improve code style

--- BinaryTree.playground/Pages/99 Recover Binary Search Tree.xcplaygroundpage/Contents.swift
+++ BinaryTree.playground/Pages/99 Recover Binary Search Tree.xcplaygroundpage/Contents.swift
@@ -1,15 +1,15 @@
 class Solution {
-    var bigger: TreeNode? = nil
-    var smaller: TreeNode? = nil
+    var bigger: TreeNode!
+    var smaller: TreeNode!
     var node = TreeNode(Int.min)
     func recoverTree(_ root: TreeNode?) {
         if root == nil {
             return
         }
         inorderTraverse(root)
-        let temp = smaller!.val
-        smaller!.val = bigger!.val
-        bigger!.val = temp
+        let temp = smaller.val
+        smaller.val = bigger.val
+        bigger.val = temp
     }
     func inorderTraverse(_ root: TreeNode?) {
         guard let root = root else {
