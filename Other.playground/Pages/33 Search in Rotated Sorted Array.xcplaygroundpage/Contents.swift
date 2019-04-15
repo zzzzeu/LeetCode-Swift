36c126c3b9713f5e488423d34d9b3b44a416c2a2
zzzzeu <zouanei@outlook.com>
2019-04-15 17:32:34 +0800

    33: Search in Rotated Sorted Array

---
+++ Other.playground/Pages/33 Search in Rotated Sorted Array.xcplaygroundpage/Contents.swift
@@ -0,0 +1,26 @@
+class Solution {
+    func search(_ nums: [Int], _ target: Int) -> Int {
+        var left = 0
+        var right = nums.count - 1
+        while left <= right {
+            let mid = (left + right) / 2
+            if nums[mid] == target {
+                return mid
+            }
+            if nums[mid] >= nums[left] {
+                if nums[mid] > target && target >= nums[left] {
+                    right = mid - 1
+                } else {
+                    left = mid + 1
+                }
+            } else {
+                if nums[mid] < target && target <= nums[right] {
+                    left = mid + 1
+                } else {
+                    right = mid - 1
+                }
+            }
+        }
+        return -1
+    }
+}
